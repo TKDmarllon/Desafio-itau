26812f6f55d3c1cde28c8b77420c6279d6631fad
TKDmarllon <marllon.rocha@hotmail.com>
2025-06-19 20:08:27 -0300

    TransactionRequest

---
+++ src/main/java/Desafio/itau/back/end/dto/TransactionRequest.java
@@ -0,0 +1,23 @@
+package Desafio.itau.back.end.dto;
+import java.time.OffsetDateTime;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public class TransactionRequest {
+    
+    @NotNull
+    @Min(0)
+    private double valor;
+    
+    @NotNull
+    private OffsetDateTime dataHora;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public OffsetDateTime getDataHora() {
+        return dataHora;
+    }
+}
